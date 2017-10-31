65e3f6dffde96c1ae80d2cf336781ff41f68d8c7
shohan494 <shohan12224943@gmail.com>
2017-10-31 09:14:21 +0600

    server with url

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 
-res.writeHead(200);
+//res.writeHead(200);
+res.writeHead(200, {"Content-Type": "text/html"});
 
-res.end('Hi everybody!');
+//res.end('Hi everybody!');
+res.end('<p>Here is a paragraph of <strong>HTML</strong>!</p>');
 
 });
 
@@ -13,16 +15,9 @@ server.listen(8080);
 
 /*
 // Code exactly the same as the previous one
-
-
 var instructionsNewVisitor = function(req, res) {
-
 res.writeHead(200);
-
 res.end('Hi everybody!');
-
 }
-
-
 var server = http.createServer(instructionsNewVisitor);
 */
