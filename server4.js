aa7993b89e6c65fa053a700e52858ef005a6dfa3
shohan494 <shohan12224943@gmail.com>
2017-11-02 08:28:44 +0600

    server with event

--- server4.js
+++ server4.js
@@ -1,12 +1,8 @@
 var http = require('http');
-
 var url = require('url');
-
 var querystring = require('querystring');
 
-
 var server = http.createServer(function(req, res) {
-
     var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "text/plain"});
 
@@ -18,7 +14,5 @@ var server = http.createServer(function(req, res) {
     }
     res.end();
     console.log(params);
-
 });
-
 server.listen(8080);
